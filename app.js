fc23cda36a7cb6f3cad7fad5e66bf9549898ca2c
argo20k <148632221+argo20k@users.noreply.github.com>
2023-11-28 17:22:52 +1000

    elements + links + wrappers
    + finished most base elements, including checkboxes
    + linked interaction between direction and alignment
    + started adding wrappers, and working on submenus

--- app.js
+++ app.js
@@ -1,38 +1,67 @@
-// Handling interactions with button groups
+// Button-groups
 const buttonGroups = document.querySelectorAll('.button-group');
-
 buttonGroups.forEach((buttonGroup) => {
 	const buttons = buttonGroup.querySelectorAll('button');
 
 	buttons.forEach((button) => {
 		button.addEventListener('click', () => {
 			buttons.forEach((button) => {
+				// Removes active styles for button
 				button.classList.remove('active');
 			});
+			// Adds active styles for button
 			button.classList.add('active');
 		});
 	});
 });
 
-// Handling interactions with dropdown picker, and option buttons
+// Dropdowns
 const dropdowns = document.querySelectorAll('.dropdown-container');
-
 dropdowns.forEach((dropdown) => {
-	const picker = dropdown.querySelector('.dropdown-picker');
+	const picker = dropdown.querySelector('.dropdown.picker');
 	const pickerTextEl = picker.querySelector('.text');
-	const optionList = dropdown.querySelector('.dropdown-option-list');
-	const options = dropdown.querySelectorAll('.dropdown-option-list .dropdown-option');
+	const optionsMenu = dropdown.querySelector('.dropdown-options-menu');
+	const options = dropdown.querySelectorAll('.dropdown-options-menu .dropdown.option');
 
 	picker.addEventListener('click', () => {
-		// Add the active styles to the dropdown-option-list element
-		optionList.classList.toggle('active');
+		// Toggles open styles for dropdown-options-menu
+		optionsMenu.classList.toggle('open');
 	});
 
 	options.forEach((option) => {
+		// Triggers when an option is selecte from the optionsMenu
 		option.addEventListener('click', () => {
 			const optionTextEl = option.querySelector('.text');
+			// Sets dropdown-picker text to the text of the chosen dropdown-option
 			pickerTextEl.innerText = optionTextEl.innerText;
-			optionList.classList.remove('active');
+
+			// Removes open styles for dropdown-options-menu
+			optionsMenu.classList.remove('open');
 		});
+	});
+});
+
+// Direction Buttons + Alignment Button-group Links
+const directionButtons = document.querySelectorAll('.button-group.direction button');
+const alignmentButtonGroup = document.querySelector('.button-group.alignment');
+directionButtons.forEach((directionButton) => {
+	directionButton.addEventListener('click', () => {
+		// Removes vertical + horizontal + reverse styles, if enabled
+		alignmentButtonGroup.classList.remove('vertical');
+		alignmentButtonGroup.classList.remove('horizontal');
+		alignmentButtonGroup.classList.remove('reverse');
+
+		// Adds appropriate styles for respective direction buttons that are clicked
+		if (directionButton.id === 'row') {
+			alignmentButtonGroup.classList.add('vertical');
+		} else if (directionButton.id === 'row-reverse') {
+			alignmentButtonGroup.classList.add('vertical');
+			alignmentButtonGroup.classList.add('reverse');
+		} else if (directionButton.id === 'column') {
+			alignmentButtonGroup.classList.add('horizontal');
+		} else if (directionButton.id === 'column-reverse') {
+			alignmentButtonGroup.classList.add('horizontal');
+			alignmentButtonGroup.classList.add('reverse');
+		}
 	});
 });
