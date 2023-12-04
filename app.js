756a6e06b7c9bf47b67e78c78b1fe0531edafc76
argo20k <148632221+argo20k@users.noreply.github.com>
2023-12-04 17:00:53 +1000

    +alignmentAndDirectionWrapper

--- app.js
+++ app.js
@@ -14,10 +14,9 @@ function elementFromHtml(html) {
 
 //
 //
-//
+// Button-groups
 
 const buttonGroups = document.querySelectorAll('.button-group');
-
 buttonGroups.forEach((buttonGroup) => {
 	var buttons = buttonGroup.querySelectorAll('button');
 
@@ -139,6 +138,176 @@ inputWrappers.forEach((inputWrapper) => {
 		input.classList.add('autoSize');
 		onInputDelegate({ target: input });
 	});
+});
+
+//
+//
+// Direction Wrapper
+
+function updateDirectionButtonGroup(buttonGroup) {
+	let directionOptions = buttonGroup.querySelectorAll('button.direction');
+	directionOptions.forEach((directionOptionButton) => {
+		// sets the selected-option of the direction button-group
+		if (directionOptionButton.dataset.state === 'selected') {
+			let directionButtonGroup = directionOptionButton.parentNode;
+			directionButtonGroup.dataset.selectedOption = directionOptionButton.dataset.option;
+		}
+	});
+}
+
+const directionWrappers = document.querySelectorAll('.wrapper.direction');
+directionWrappers.forEach((directionWrapper) => {
+	let directionOptions = directionWrapper.querySelectorAll('button.direction');
+
+	directionOptions.forEach((directionOptionButton) => {
+		directionOptionButton.addEventListener('click', () => {
+			updateDirectionButtonGroup(directionWrapper);
+		});
+	});
+
+	updateDirectionButtonGroup(directionWrapper);
+});
+
+//
+//
+// Alignment Wrapper
+
+function updateAlignmentButtonGroup(buttonGroup) {
+	let alignmentOptions = buttonGroup.querySelectorAll('button.alignment');
+	alignmentOptions.forEach((alignmentOptionButton) => {
+		// sets the selected-option of the alignment button-group
+		if (alignmentOptionButton.dataset.state === 'selected') {
+			let alignmentSelectedOption = `column-${alignmentOptionButton.dataset.column}, row-${alignmentOptionButton.dataset.row}`;
+			buttonGroup.dataset.selectedOption = alignmentSelectedOption;
+		}
+	});
+}
+
+const alignmentWrappers = document.querySelectorAll('.wrapper.alignment');
+alignmentWrappers.forEach((alignmentWrapper) => {
+	var alignmentOptions = alignmentWrapper.querySelectorAll('button.alignment');
+	let columnIndex = 0;
+	let rowIndex = 0;
+	alignmentOptions.forEach((alignmentOptionButton, index) => {
+		columnIndex = (index % 3) + 1;
+		if (index % 3 == 0) {
+			rowIndex++;
+		}
+		alignmentOptionButton.dataset.column = columnIndex;
+		alignmentOptionButton.dataset.row = rowIndex;
+
+		alignmentOptionButton.addEventListener('click', () => {
+			updateAlignmentButtonGroup(alignmentWrapper);
+		});
+	});
+	updateAlignmentButtonGroup(alignmentWrapper);
+});
+
+//
+//
+// Alignment Buttons
+
+const alignmentButtons = document.querySelectorAll('button.alignment');
+alignmentButtons.forEach((alignmentButton) => {
+	alignmentButton.addEventListener('mouseover', () => {
+		if (!alignmentButton.dataset.state) {
+			alignmentButton.dataset.state = 'hovering';
+		}
+	});
+	alignmentButton.addEventListener('mouseout', () => {
+		if (alignmentButton.dataset.state === 'hovering') {
+			alignmentButton.removeAttribute('data-state');
+		}
+	});
+
+	// alignmentButton.innerHTML = '<span class="icon-circle-small"></span>';
+});
+
+//
+//
+// Direction and Alignment
+
+function updateDAAWrapper(dAAWrapper) {
+	var directionWrapper = dAAWrapper.querySelector('.wrapper.direction');
+	dAAWrapper.dataset.direction = directionWrapper.dataset.selectedOption;
+	var alignmentWrapper = dAAWrapper.querySelector('.wrapper.alignment');
+	dAAWrapper.dataset.alignment = alignmentWrapper.dataset.selectedOption;
+
+	updateAlignmentOptionButtonIcon(dAAWrapper);
+}
+
+function updateAlignmentOptionButtonIcon(dAAWrapper) {
+	var alignmentOptions = dAAWrapper.querySelectorAll('button.alignment');
+	var iconHtml = {
+		'h-left': '<span class="icon-alignment-horizontal-left"></span><span class="icon-circle-small"></span>',
+		'h-center': '<span class="icon-alignment-horizontal-center"></span><span class="icon-circle-small"></span>',
+		'h-right': '<span class="icon-alignment-horizontal-right"></span><span class="icon-circle-small"></span>',
+		'h-left-r': '<span class="icon-alignment-horizontal-left-reverse"></span><span class="icon-circle-small"></span>',
+		'h-center-r': '<span class="icon-alignment-horizontal-center-reverse"></span><span class="icon-circle-small"></span>',
+		'h-right-r': '<span class="icon-alignment-horizontal-right-reverse"></span><span class="icon-circle-small"></span>',
+		'v-top': '<span class="icon-alignment-vertical-top"></span><span class="icon-circle-small"></span>',
+		'v-center': '<span class="icon-alignment-vertical-center"></span><span class="icon-circle-small"></span>',
+		'v-bottom': '<span class="icon-alignment-vertical-bottom"></span><span class="icon-circle-small"></span>',
+		'v-top-r': '<span class="icon-alignment-vertical-top-reverse"></span><span class="icon-circle-small"></span>',
+		'v-center-r': '<span class="icon-alignment-vertical-center-reverse"></span><span class="icon-circle-small"></span>',
+		'v-bottom-r': '<span class="icon-alignment-vertical-bottom-reverse"></span><span class="icon-circle-small"></span>',
+	};
+
+	alignmentOptions.forEach((alignmentButton) => {
+		// alignmentButton.innerHTML = '<span class="icon-circle-small"></span>';
+		if (dAAWrapper.dataset.direction === 'column') {
+			if (alignmentButton.dataset.column === '1') {
+				alignmentButton.innerHTML = iconHtml['h-left'];
+			} else if (alignmentButton.dataset.column === '2') {
+				alignmentButton.innerHTML = iconHtml['h-center'];
+			} else if (alignmentButton.dataset.column === '3') {
+				alignmentButton.innerHTML = iconHtml['h-right'];
+			}
+		} else if (dAAWrapper.dataset.direction === 'row') {
+			if (alignmentButton.dataset.row === '1') {
+				alignmentButton.innerHTML = iconHtml['v-top'];
+			} else if (alignmentButton.dataset.row === '2') {
+				alignmentButton.innerHTML = iconHtml['v-center'];
+			} else if (alignmentButton.dataset.row === '3') {
+				alignmentButton.innerHTML = iconHtml['v-bottom'];
+			}
+		} else if (dAAWrapper.dataset.direction === 'column-reverse') {
+			if (alignmentButton.dataset.column === '1') {
+				alignmentButton.innerHTML = iconHtml['h-left-r'];
+			} else if (alignmentButton.dataset.column === '2') {
+				alignmentButton.innerHTML = iconHtml['h-center-r'];
+			} else if (alignmentButton.dataset.column === '3') {
+				alignmentButton.innerHTML = iconHtml['h-right-r'];
+			}
+		} else if (dAAWrapper.dataset.direction === 'row-reverse') {
+			if (alignmentButton.dataset.row === '1') {
+				alignmentButton.innerHTML = iconHtml['v-top-r'];
+			} else if (alignmentButton.dataset.row === '2') {
+				alignmentButton.innerHTML = iconHtml['v-center-r'];
+			} else if (alignmentButton.dataset.row === '3') {
+				alignmentButton.innerHTML = iconHtml['v-bottom-r'];
+			}
+		}
+	});
+}
+
+const directionAndAlignmentWrappers = document.querySelectorAll('.wrapper.direction-and-alignment');
+directionAndAlignmentWrappers.forEach((dAAWrapper) => {
+	var directionOptions = dAAWrapper.querySelectorAll('button.direction');
+	directionOptions.forEach((directionOptionButton) => {
+		directionOptionButton.addEventListener('click', () => {
+			updateDAAWrapper(dAAWrapper);
+
+			// update direction of alignment icons
+		});
+	});
+	var alignmentOptions = dAAWrapper.querySelectorAll('button.alignment');
+	alignmentOptions.forEach((alignmentOptionButton) => {
+		alignmentOptionButton.addEventListener('click', () => {
+			updateDAAWrapper(dAAWrapper);
+		});
+	});
+	updateDAAWrapper(dAAWrapper);
 });
 
 //
